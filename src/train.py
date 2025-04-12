ef44832246bffb0fbf18753c4a06de3ef77db6eb
AHUT-MILAGroup <milagit@qq.com>
2025-04-12 11:32:19 +0800

    <commit contain>

---
+++ src/train.py
@@ -0,0 +1,9 @@
+from config.train import TrainConfig
+from src.utils.helper import run
+from src.utils.trainEUC import train1
+# from src.utils.train_normal import train1 
+# from src.utils.trainPCCD import train1
+
+if __name__ == '__main__':
+    config = TrainConfig()
+    run(train1, config)
